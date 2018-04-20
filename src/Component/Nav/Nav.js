45cf8a6355155e8249dcafad9a74279f11975230
reaganforonda <foronda.reagan@gmail.com>
2018-04-20 11:46:46 -0600

    completed step 3 part 1

--- src/Component/Nav/Nav.js
+++ src/Component/Nav/Nav.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-export default function Nav(props){
-    return (
-        <div>
-            <Link to='/dashboard'><button>Home</button></Link>
-            <Link to='/Post'><button>New Post</button></Link>
-            <Link to='/'><button>Logout</button></Link>
-        </div>
-    )
+export class Nav extends React.Component{
+    render(){
+        console.log(this.props);
+        const {username, img} = this.props;
+        return (
+            <div>
+                <img src={img} alt=''/>
+                <p>{username}</p>
+                <Link to='/dashboard'><button>Home</button></Link>
+                <Link to='/Post'><button>New Post</button></Link>
+                <Link to='/'><button>Logout</button></Link>
+            </div>
+        )
+    }
+    
 }
+
+function mapStateToProps(state){
+    return {
+        username : state.username,
+        img : state.img
+    };
+}
+
+export default connect(mapStateToProps, {})(Nav);
+  
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,23 +1,29 @@
-const INITIAL_STATE = {}
-
-// ####################
-// ### Action Types ###
-// ####################
-
-
-// ####################
-// ### Action Types ###
-// ####################
-
-
-
-export default function reducer(state = INITIAL_STATE, action){
-    switch(action.type){
-        /*
-         * INSERT casees
-         * 
-        */
-        default:
-        return state;
-    }
+const initialState = {
+    username : 'sdfsd',
+    id : '',
+    img : 'http://www.fillmurray.com/100/100'
 }
+
+// ####################
+// ### Action Types ###
+// ####################
+
+
+// ####################
+// ### Action Types ###
+// ####################
+
+
+
+export default function reducer(state = initialState, action){
+    // switch(action.type){
+    //     /*
+    //      * INSERT casees
+    //      * 
+    //     */
+    //     default:
+    //     return state;
+    // }
+
+    return state;
+};
